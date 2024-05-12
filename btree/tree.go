befbbc86bbd653603d97a449a067595bf407ff20
Ritesh Koushik <riteshkoushik39@gmail.com>
2024-05-12 19:28:48 +0530

    Setup ptrs, KV and offset list

--- btree/tree.go
+++ btree/tree.go
@@ -1,8 +1,6 @@
 package btree
 
-import(
-    "assert"
-)
+import "encoding/binary"
 
 type BNode struct {
     data []byte // to be dumped to disk
@@ -29,51 +27,85 @@ const BTREE_MAX_VAL_SIZE = 3000
 
 func init() {
     node1max := HEADER + 8 + 2 + 4 + BTREE_MAX_KEY_SIZE + BTREE_MAX_VAL_SIZE
-    assert(node1max <= BTREE_PAGE_SIZE)
+    assert(node1max > BTREE_MAX_KEY_SIZE, "Node size exceeds allowed limit")
+
+    // if node1max > BTREE_PAGE_SIZE {
+        // Handle error: Node size exceeds page size limit
+        // assertion did not work properly
+
+        // panic("Node size exceeds allowed limit")
+        // assert.True(node1max <= BTREE_PAGE_SIZE)
+    // }
+}
+
+func assert(condition bool, msg string){
+    if condition != true{
+        panic(msg)
+    }
 }
 
 // header
 func (node BNode) btype() uint16 {
-
+    return binary.LittleEndian.Uint16(node.data)
 }
 func (node BNode) nkeys() uint16 {
-
+    return binary.LittleEndian.Uint16(node.data[2:4])
 }
 
 func (node BNode) setHeader(btype uint16, nkeys uint16) {
-
+    binary.LittleEndian.PutUint16(node.data[0:2], btype)
+    binary.LittleEndian.PutUint16(node.data[2:4], nkeys)
 }
 
 // pointers
 func (node BNode) getPtr(idx uint16) uint64 {
-
+    assert(idx < node.nkeys(), "idx not less than nkeys") 
+    pos := HEADER + 8 * idx
+    return binary.LittleEndian.Uint64(node.data[pos:])
 }
 func (node BNode) setPtr(idx uint16, val uint64) {
-
+    assert(idx < node.nkeys(), "idx not less than nkeys") 
+    pos := HEADER + 8 * idx
+    binary.LittleEndian.PutUint64(node.data[pos:], val)
 }
 
 // offset list - used to locate nth KV pair quickly
 func offsetPos(node BNode, idx uint16) uint16 {
-
+    assert(1 <= idx && idx <= node.nkeys(), "idx not in range (1, nkeys)")
+    return HEADER + 8 * node.nkeys() + 2 * (idx - 1)
 }
 func (node BNode) getOffSet(idx uint16) uint16 {
-
+    if idx == 0 {
+        return 0
+    }
+    return binary.LittleEndian.Uint16(node.data[offsetPos(node, idx):])
+}
+func (node BNode) setOffset(idx uint16, offset uint16){
+    binary.LittleEndian.PutUint16(node.data[offsetPos(node, idx):], offset)
 }
 
 // key-values (KV)
 func (node BNode) kvPos(idx uint16) uint16 {
-
+    assert(idx <= node.nkeys(), "idx not less than nkeys")
+    return HEADER + 8 * node.nkeys() + 2 * node.nkeys() + node.getOffSet(idx)
 }
 func (node BNode) getKey(idx uint16) []byte {
-
+    assert(idx < node.nkeys(), "idx not less than nkeys")
+    pos := node.kvPos(idx)
+    klen := binary.LittleEndian.Uint16(node.data[pos:])
+    return node.data[pos + 4:][:klen]
 }
 func (node BNode) getVal(idx uint16) []byte {
-
+    assert(idx < node.nkeys(), "idx not less than nkeys")    
+    pos := node.kvPos(idx)
+    klen := binary.LittleEndian.Uint16(node.data[pos + 0:])
+    vlen := binary.LittleEndian.Uint16(node.data[pos + 2:])
+    return node.data[pos + 4 + klen:][:vlen]
 }
 
 // node size in bytes
 func (node BNode) nbytes() uint16 {
-
+    return node.kvPos(node.nkeys())
 }
 
 // B-Tree Insertion
